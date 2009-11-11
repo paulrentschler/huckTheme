d3966e62f5c0619af789bc5ebc3709136ca57be8
par117 <par117@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-11-11 20:37:52 +0000

    Customized the collection portlet to display more info for news items and events.

--- huckInstitutes/Huck/HuckTheme/Branches/modernRetro/browser/collection.py
+++
@@ -1,8 +0,0 @@
-from plone.portlet.collection.collection import Renderer
-
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-
-class myCollectionRenderer(Renderer):
-
-     _template = ViewPageTemplateFile('templates/collection.pt')
