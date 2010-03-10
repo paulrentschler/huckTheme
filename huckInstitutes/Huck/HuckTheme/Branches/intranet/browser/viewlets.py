cca09b9dc4a2fa5aced46a1672b756982a520afa
par117 <par117@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2010-03-10 19:40:09 +0000

    content bar colors, config menu in personal bar, hide manage portlet links, restyle global nav, remove old theme folders

--- huckInstitutes/Huck/HuckTheme/Branches/intranet/browser/viewlets.py
+++ huckInstitutes/Huck/HuckTheme/Branches/intranet/browser/viewlets.py
@@ -2,6 +2,7 @@ from zope.component import getMultiAdapter
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 from plone.app.layout.viewlets import common
+from Products.CMFCore.utils import getToolByName
 
 class LogoViewlet(ViewletBase):
     render = ViewPageTemplateFile('templates/logo.pt')
@@ -20,3 +21,13 @@ class SearchBoxViewlet(common.SearchBoxViewlet):
 
 class PathBarViewlet(common.PathBarViewlet):
     render = ViewPageTemplateFile('templates/path_bar.pt')
+
+class PersonalBarViewlet(common.PersonalBarViewlet):
+    render = ViewPageTemplateFile('templates/personal_bar.pt')
+    
+    def update(self):
+        common.PersonalBarViewlet.update(self)
+        
+        mt = getToolByName(self.context, 'portal_membership')
+        self.canManagePortlets = mt.checkPermission('Portlets: Manage portlets', self.context)
+        self.canManagePortal = mt.checkPermission('Manage portal', self.context)
