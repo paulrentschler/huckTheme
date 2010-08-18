7be45a240485228e2d9654c7f452f8557de8a783
par117 <par117@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2010-08-18 14:19:08 +0000

    added custom personal bar, hid some more viewlets

--- huckInstitutes/Huck/HuckTheme/trunk/Products/HuckTheme/browser/viewlets.py
+++ huckInstitutes/Huck/HuckTheme/trunk/Products/HuckTheme/browser/viewlets.py
@@ -2,6 +2,7 @@ from zope.component import getMultiAdapter
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 from plone.app.layout.viewlets import common
+from Products.CMFCore.utils import getToolByName
 
 class LogoViewlet(ViewletBase):
     index = ViewPageTemplateFile('templates/logo.pt')
@@ -16,3 +17,12 @@ class SearchBoxViewlet(common.SearchBoxViewlet):
 
 class SiteActionsViewlet(common.SiteActionsViewlet):
     index = ViewPageTemplateFile('templates/site_actions.pt')
+
+class PersonalBarViewlet(common.PersonalBarViewlet):
+    index = ViewPageTemplateFile('templates/personal_bar.pt')
+
+    def update(self):
+        common.PersonalBarViewlet.update(self)
+        mt = getToolByName(self.context, 'portal_membership')
+        self.canManagePortlets = mt.checkPermission('Portlets: Manage portlets', self.context)
+        self.canManagePortal = mt.checkPermission('Manage portal', self.context)
