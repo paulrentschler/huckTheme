2554e66243a540b78b7a35eae5718ca38beb5408
Paul Rentschler <paul@rentschler.ws>
2010-08-18 13:51:14 +0000

    viewlets reordered and working, appears to uninstall cleanly

--- Products/HuckTheme/browser/viewlets.py
+++ Products/HuckTheme/browser/viewlets.py
@@ -4,9 +4,15 @@ from plone.app.layout.viewlets.common import ViewletBase
 from plone.app.layout.viewlets import common
 
 class LogoViewlet(ViewletBase):
-    render = ViewPageTemplateFile('templates/logo.pt')
+    index = ViewPageTemplateFile('templates/logo.pt')
 
     def update(self):
         portal_state = getMultiAdapter((self.context, self.request),
                                             name=u'plone_portal_state')
         self.navigation_root_url = portal_state.navigation_root_url()
+
+class SearchBoxViewlet(common.SearchBoxViewlet):
+    index = ViewPageTemplateFile('templates/searchbox.pt')
+
+class SiteActionsViewlet(common.SiteActionsViewlet):
+    index = ViewPageTemplateFile('templates/site_actions.pt')
