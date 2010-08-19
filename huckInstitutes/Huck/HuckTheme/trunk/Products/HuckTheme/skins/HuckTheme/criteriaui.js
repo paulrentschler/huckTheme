4248b2b2895bf985f0e5ac9f413d6dc986aaaf81
par117 <par117@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2010-08-19 13:52:38 +0000

    added the javascript files, a default section logo, and customized the author page template

---
+++ huckInstitutes/Huck/HuckTheme/trunk/Products/HuckTheme/skins/HuckTheme/criteriaui.js
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * Written by: Paul Rentschler
+ * Created on: 10 August 2010
+ *
+ * Description: Makes setting collection criteria easier by making all
+ *               select boxes have a height of 15 instead of the standard 6
+ ******************************************************************************/
+
+
+jq(document).ready(
+    /* when the document loads, grab all of the select boxes in the main
+       criteria table area and set their size to 15
+       */
+       
+    function () {
+        // make all the select boxes a height of 15
+        jq('table.listing select').attr('size', '15')
+    }
+);
