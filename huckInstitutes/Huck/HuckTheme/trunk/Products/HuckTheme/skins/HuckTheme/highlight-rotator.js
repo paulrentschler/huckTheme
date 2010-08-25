e97cda5530d27f0a89d3f880a909e8b5ad20fab7
par117 <par117@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2010-08-25 17:50:59 +0000

    captions working properly with image rotator

--- huckInstitutes/Huck/HuckTheme/trunk/Products/HuckTheme/skins/HuckTheme/highlight-rotator.js
+++ huckInstitutes/Huck/HuckTheme/trunk/Products/HuckTheme/skins/HuckTheme/highlight-rotator.js
@@ -14,18 +14,6 @@ jq(document).ready(function() {
        and setup how to rotate through the images
        */
     
-    // show the paging box and activate the first link
-    jq('.highlight-paging').show();
-    jq('.highlight-paging a:first').addClass('active');
-    
-    // get the size of the images, how many there are, and determine the overall width
-    var imageWidth = jq('.highlight-window').width();
-    var imageSum = jq('.highlight-list li').size();
-    var imageReelWidth = imageWidth * imageSum;
-    
-    // adjust the image reel (highlight-list) to it's new size
-    jq('.highlight-list').width(imageReelWidth);
-    
     
     /*** Define the functions necessary to handle the rotating ***/
     
@@ -50,6 +38,23 @@ jq(document).ready(function() {
         } else {
             jq('.highlight-list').animate( {left: -imageReelPosition}, 600 );
         }
+        
+        // update the caption
+        var $activeCaption = jq('#' + $active.attr('container') + '-item' + $active.attr('rel') + ' div.highlight-caption');
+        var $captionBox = jq('.highlight-caption-box');
+        var currentHeight = $captionBox.height();
+        $captionBox.animate( {opacity: 0}, 250, function() {
+            $captionBox.html($activeCaption.clone());
+            jq('.highlight-caption-box div').show();
+            $captionBox.animate( {opacity: 0.80}, 250 );
+            if ($captionBox.is(':hidden')) {
+                // show the animated caption box
+                $captionBox.show();
+                
+                // hide the individual captions
+                jq('.highlight-list .highlight-caption').hide();
+            }
+        });
     };
 
 
@@ -72,8 +77,6 @@ jq(document).ready(function() {
     }
     
     
-    // start the rotation
-    rotateSwitch();
 
 
 
@@ -101,6 +104,26 @@ jq(document).ready(function() {
         // prevent the browser from going to the link anchor
         return false;
     });
+    
+    
+    
+    
+    // get the size of the images, how many there are, and determine the overall width
+    var imageWidth = jq('.highlight-window').width();
+    var imageSum = jq('.highlight-list li').size();
+    var imageReelWidth = imageWidth * imageSum;
+    
+    // adjust the image reel (highlight-list) to it's new size
+    jq('.highlight-list').width(imageReelWidth);
+    
+    // show the paging box and activate the first link
+    jq('.highlight-paging').show();
+    $active = jq('.highlight-paging a:first');
+    rotate();
+    
+    // start the rotation
+    rotateSwitch();
+
 });
 
 
