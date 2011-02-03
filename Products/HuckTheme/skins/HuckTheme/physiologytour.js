920ca7867b2bdc3cda5ba8392f4dc8c31c412c09
Paul Rentschler <paul@rentschler.ws>
2011-02-03 22:02:42 +0000

    Moved the flash files

--- Products/HuckTheme/skins/HuckTheme/physiologytour.js
+++ Products/HuckTheme/skins/HuckTheme/physiologytour.js
@@ -14,7 +14,7 @@ jq(document).ready( function () {
   jq("div.video-right").prepend('<a id="tour-launcher" href=""><img src="/education/physiology/images/interactive-tour.jpg" width="300" height="172" alt="Click this image of a translucent head for a flash-based interactive tour of the Physiology Graduate program. All content is also available through the various pages of this web site." /></a>');
 
   // insert the overlay that will hold the flash tour
-  jq("body").append('<div id="showcase-tour"><a href="" class="close" id="showcase-tour-close"></a><object type="application/x-shockwave-flash" data="/education/physiology/showcase.swf/index_html" width="740" height="550" bgcolor="#ffffff"><param name="movie" value="/education/physiology/showcase.swf/index_html" bgcolor="transparent" /><param name="bgcolor" value="#ffffff" /><param name="wmode" value="transparent" /></object></div>');
+  jq("body").append('<div id="showcase-tour"><a href="" class="close" id="showcase-tour-close"></a><object type="application/x-shockwave-flash" data="/physio-tour/showcase.swf" width="740" height="550" bgcolor="#ffffff"><param name="movie" value="/physio-tour/showcase.swf" bgcolor="transparent" /><param name="bgcolor" value="#ffffff" /><param name="wmode" value="transparent" /></object></div>');
 
   // assign the click events
   jq("#tour-launcher").click( function (e) {
