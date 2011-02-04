31ba57b4e73bdb1836c3179a9fdd8321ed6181b7
Paul Rentschler <paul@rentschler.ws>
2011-02-04 15:53:37 +0000

    Added a check for flash support to the js.

--- Products/HuckTheme/skins/HuckTheme/physiologytour.js
+++ Products/HuckTheme/skins/HuckTheme/physiologytour.js
@@ -10,24 +10,27 @@
 
 jq(document).ready( function () {
 
-  // insert the launching image
-  jq("div.video-right").prepend('<a id="tour-launcher" href=""><img src="/education/physiology/images/interactive-tour.jpg" width="300" height="172" alt="Click this image of a translucent head for a flash-based interactive tour of the Physiology Graduate program. All content is also available through the various pages of this web site." /></a>');
+  // test for flash support
+  if (navigator.mimeTypes && navigator.mimeTypes["application/x-shockwave-flash"]) {
+    // insert the launching image
+    jq("div.video-right").prepend('<a id="tour-launcher" href=""><img src="/education/physiology/images/interactive-tour.jpg" width="300" height="172" alt="Click this image of a translucent head for a flash-based interactive tour of the Physiology Graduate program. All content is also available through the various pages of this web site." /></a>');
 
-  // insert the overlay that will hold the flash tour
-  jq("body").append('<div id="showcase-tour"><a href="" class="close" id="showcase-tour-close"></a><object type="application/x-shockwave-flash" data="/physio-tour/showcase.swf" width="740" height="550" bgcolor="#ffffff"><param name="movie" value="/physio-tour/showcase.swf" bgcolor="transparent" /><param name="bgcolor" value="#ffffff" /><param name="wmode" value="transparent" /></object></div>');
+    // insert the overlay that will hold the flash tour
+    jq("body").append('<div id="showcase-tour"><a href="" class="close" id="showcase-tour-close"></a><object type="application/x-shockwave-flash" data="/physio-tour/showcase.swf" width="740" height="550" bgcolor="#ffffff"><param name="movie" value="/physio-tour/showcase.swf" bgcolor="transparent" /><param name="bgcolor" value="#ffffff" /><param name="wmode" value="transparent" /></object></div>');
 
-  // assign the click events
-  jq("#tour-launcher").click( function (e) {
-    e.preventDefault();
-    var leftPos = (jq(window).width() - jq("#showcase-tour").width()) / 2;
-    jq("#showcase-tour").css("left", leftPos).show();
-    jq(".video-right object").hide();
-  });
+    // assign the click events
+    jq("#tour-launcher").click( function (e) {
+      e.preventDefault();
+      var leftPos = (jq(window).width() - jq("#showcase-tour").width()) / 2;
+      jq("#showcase-tour").css("left", leftPos).show();
+      jq(".video-right object").hide();
+    });
 
-  jq("a#showcase-tour-close").click( function (e) {
-    e.preventDefault();
-    jq("#showcase-tour").hide();
-    jq(".video-right object").show();
-  });
+    jq("a#showcase-tour-close").click( function (e) {
+      e.preventDefault();
+      jq("#showcase-tour").hide();
+      jq(".video-right object").show();
+    });
+  }
 
 });
