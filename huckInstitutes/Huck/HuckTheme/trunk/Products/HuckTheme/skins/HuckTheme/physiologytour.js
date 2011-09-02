7ca92b827ae4b7fa7b6ceda0bf620c6b4f87843e
par117 <par117@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2011-09-02 15:18:15 +0000

    Updated the image used to launch the physiology flash site.

--- huckInstitutes/Huck/HuckTheme/trunk/Products/HuckTheme/skins/HuckTheme/physiologytour.js
+++ huckInstitutes/Huck/HuckTheme/trunk/Products/HuckTheme/skins/HuckTheme/physiologytour.js
@@ -28,7 +28,7 @@ jq(document).ready( function () {
   
   if (flashSupported) {
     // insert the launching image
-    jq("div.video-right").prepend('<a id="tour-launcher" href=""><img src="/education/physiology/images/interactive-tour.jpg" width="300" height="50" alt="Image of a translucent head on a DNA strand background used to launch a flash-based interactive tour of the Physiology Graduate program. All content is also available through the various pages of this web site." /></a>');
+    jq("div.video-right").prepend('<a id="tour-launcher" href=""><img src="/education/physiology/images/tour-launch-g.jpg" width="300" height="50" alt="Image of a translucent head on a DNA strand background used to launch a flash-based interactive tour of the Physiology Graduate program. All content is also available through the various pages of this web site." /></a>');
 
     // insert the overlay that will hold the flash tour
     jq("body").append('<div id="showcase-tour"><a href="" class="close" id="showcase-tour-close"></a><iframe src="/physio-tour/index.html" width="740" height="550" frameborder="0"><p>Sorry, your browser does not support the use of inline frames (iframes).</p><p>All the content contained in this showcase is available through our web site.</p></iframe></div>');
