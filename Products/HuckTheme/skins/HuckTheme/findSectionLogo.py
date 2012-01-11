034d84a841427221750005fc0364184ad23e5350
Paul Rentschler <paul@rentschler.ws>
2012-01-11 15:29:30 +0000

    added alt text to the section logos

--- Products/HuckTheme/skins/HuckTheme/findSectionLogo.py
+++ Products/HuckTheme/skins/HuckTheme/findSectionLogo.py
@@ -6,13 +6,18 @@
 #                the section logo image and returns the object for display
 #
 
+# set the default alt text
+altText = "Graphic logo for the current section of the website"
+
 # define the default logo
 try:
     defaultLogo = context.logos['section-logo-default.jpg']
     logoResult = defaultLogo.absolute_url()
+    altText = "Students walk past the Life Sciences building on the Shortledge Mall"
 except:
     defaultLogo = 'http://www.huck.psu.edu/_images/section-logo-default.jpg'
     logoResult = defaultLogo
+    altText = "Students walk past the Life Sciences building on the Shortledge Mall"
 
 
 # break the absolute url into pieces at the slash (/)
@@ -24,33 +29,41 @@ section = context.getSectionFromURL().replace('section-', '')
 
 # if there is a section, look for a logo
 if section <> "":
-    sectionIndex = urlParts.index(section)
+    try:
+        sectionIndex = urlParts.index(section)
     
-    # make sure the url goes beyond the major section
-    if (len(urlParts) - 1) > sectionIndex:
-        # define the name of the section logo
-        sectionId = urlParts[sectionIndex + 1]
-        sectionLogoName = sectionId + '-logo.jpg'
+        # make sure the url goes beyond the major section
+        if (len(urlParts) - 1) > sectionIndex:
+            # define the name of the section logo
+            sectionId = urlParts[sectionIndex + 1]
+            sectionLogoName = sectionId + '-logo.jpg'
 
 
-        # see if a section logo exists in the site
-        logoObjs = context.portal_catalog(id = sectionLogoName)
-        if len(logoObjs) > 0:
-            #logoResult = logoObjs[0].getObject()
-            logoResult = ""
-            for i in range(0, sectionIndex + 2):
-                logoResult += urlParts[i] + "/"
+            # see if a section logo exists in the site
+            logoObjs = context.portal_catalog(id = sectionLogoName)
+            if len(logoObjs) > 0:
+                try:
+                    altText = logoObjs[0].Description
+                except:
+                    pass
+                    
+                #logoResult = logoObjs[0].getObject()
+                logoResult = ""
+                for i in range(0, sectionIndex + 2):
+                    logoResult += urlParts[i] + "/"
         
-            logoResult += sectionLogoName
+                logoResult += sectionLogoName
 
-        else:
-            # see if a default section logo exists
-            defaultSectionLogoName = section + '-' + sectionLogoName
-            try:
-                if defaultSectionLogoName in context.logos:
-                    logoResult = context.logos[defaultSectionLogoName].absolute_url()
-            except:
-                pass
+            else:
+                # see if a default section logo exists
+                defaultSectionLogoName = section + '-' + sectionLogoName
+                try:
+                    if defaultSectionLogoName in context.logos:
+                        logoResult = context.logos[defaultSectionLogoName].absolute_url()
+                except:
+                    pass
+    except:
+        pass
 
 # return the logo object
-return logoResult
+return (logoResult, altText)
