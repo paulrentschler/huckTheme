5de93bee95bad1c852e78d34725ae477023f191a
Paul Rentschler <paul@rentschler.ws>
2012-07-12 17:40:00 +0000

    updated the UID lookup to work in Plone 4.1.x

--- Products/HuckTheme/skins/HuckTheme/findSpecialtyForTopic.py
+++ Products/HuckTheme/skins/HuckTheme/findSpecialtyForTopic.py
@@ -7,8 +7,9 @@
 
 if (u'crit__getRawSpecialties_ATReferenceCriterion' in [c.id for c in context.listCriteria()]):
   specialtyUID = context.getCriterion('getRawSpecialties_ATReferenceCriterion').getCriteriaItems()[0][1]['query'][0]
-  #return context.archetype_tool.lookupObject(specialtyUID)
-  return context.portal_catalog(UID=specialtyUID)[0].getObject()
+  #return context.archetype_tool.lookupObject(specialtyUID) -- used in Plone 2.5?
+  #return context.portal_catalog(UID=specialtyUID)[0].getObject() -- used in Plone 3.x
+  return context.reference_catalog.lookupObject(specialtyUID)
 
 else:
   return None
